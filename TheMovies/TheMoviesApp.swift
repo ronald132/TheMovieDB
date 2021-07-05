83f2c272f177039b8e4a869f7f97d794d899f3b7
Ronald SG <ronald132@gmail.com>
2021-07-05 22:05:41 +0800

    Initial Commit

---
+++ TheMovies/TheMoviesApp.swift
@@ -0,0 +1,17 @@
+//
+//  TheMoviesApp.swift
+//  TheMovies
+//
+//  Created by Ronald on 5/7/21.
+//
+
+import SwiftUI
+
+@main
+struct TheMoviesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
